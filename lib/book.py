9a91388f9200bf463ed3837159d0af4a0c084bde
Angellucy460 <angelliona38@gmail.com>
2025-08-11 21:36:17 +0300

    books and shoe

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,45 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title="", page_count=0, author=""):
+        self.title = title
+        self.page_count = page_count
+        self.author = author
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._title = value
+        else:
+            print("Title must be a non-empty string.")
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._author = value
+        else:
+            print("Author must be a non-empty string.")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,68 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand="", size=0, color="", material="", condition=""):
+        self.brand = brand
+        self.size = size
+        self.color = color
+        self.material = material
+        self.condition = condition
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        if isinstance(value, str):
+            self._brand = value
+        else:
+            print("Brand must be a string.")
+
+    @property
+    def size(self):
+      return self._size
+    
+    @size.setter
+    def size(self, value):
+      if isinstance(value, int):
+        self._size = value
+      else:
+        print("size must be an integer")
+        
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, value):
+        if isinstance(value, str):
+            self._color = value
+        else:
+            print("Color must be a string.")
+
+    @property
+    def material(self):
+        return self._material
+
+    @material.setter
+    def material(self, value):
+        if isinstance(value, str):
+            self._material = value
+        else:
+            print("Material must be a string.")
+
+    @property
+    def condition(self):
+        return self._condition
+
+    @condition.setter
+    def condition(self, value):
+        if isinstance(value, str):
+            self._condition = value
+        else:
+            print("Condition must be a string.")
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
